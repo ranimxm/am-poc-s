8de40dc7827a8e712a9319d8a91db42fe405db24
Ranim <ranimm01@hotmail.com>
2024-10-08 13:43:33 +0200

    include production server url

--- websocket-server/render-webapp/is-localhost.js
+++ websocket-server/render-webapp/is-localhost.js
@@ -1,4 +1,4 @@
 export const isLocalhost = () => {
     const browserUrl = window.location.href;
-    return browserUrl.includes("render-webapp") ? "localhost:8080" : "";
+    return browserUrl.includes("render-webapp") ? "localhost:8080" : "https://webserver-socketio.onrender.com/";
 };
